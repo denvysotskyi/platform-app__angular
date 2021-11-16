8ee3b66903ed5b4049acc4d500d2c7364908d798
denvysotskyi <denvysotskyi@gmail.com>
2021-11-16 13:49:10 +0200

    Add store and register action

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Store } from '@ngrx/store'
+
+import { registerAction } from '../../store/actions/register.action'
 
 @Component({
   selector: 'app-register',
@@ -9,7 +12,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class RegisterComponent implements OnInit {
   form: FormGroup
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm()
@@ -17,13 +20,13 @@ export class RegisterComponent implements OnInit {
 
   initializeForm(): void {
     this.form = this.fb.group({
-      name: ['', Validators.required],
+      username: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', Validators.required]
     })
   }
 
   onSubmit(): void {
-    console.log(this.form.value)
+    this.store.dispatch(registerAction(this.form.value))
   }
 }
