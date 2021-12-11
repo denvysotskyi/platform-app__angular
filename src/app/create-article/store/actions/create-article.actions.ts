27e12c35178d103769d02c6adce14093bf473ea3
denvysotskyi <denvysotskyi@gmail.com>
2021-12-11 14:58:06 +0200

    Add create-article effect

---
+++ src/app/create-article/store/actions/create-article.actions.ts
@@ -0,0 +1,21 @@
+import { createAction, props } from '@ngrx/store'
+
+import { ActionTypes } from './action-types'
+import { ArticleInputInterface } from '../../../shared/interfaces/article-input.interface'
+import { ArticleInterface } from '../../../shared/interfaces/article.interface'
+import { BackendErrorsInterface } from '../../../shared/interfaces/backend-errors.interface'
+
+export const createArticleAction = createAction(
+  ActionTypes.CREATE_ARTICLE,
+  props<{ articleInput: ArticleInputInterface }>()
+)
+
+export const createArticleSuccessAction = createAction(
+  ActionTypes.CREATE_ARTICLE_SUCCESS,
+  props<{ article: ArticleInterface }>()
+)
+
+export const createArticleFailureAction = createAction(
+  ActionTypes.CREATE_ARTICLE_FAILURE,
+  props<{ errors: BackendErrorsInterface }>()
+)
---
+++ src/app/create-article/store/effects/create-article.effect.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core'
+import { Router } from '@angular/router'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { catchError, map, of, switchMap, tap } from 'rxjs'
+
+import {
+  createArticleAction,
+  createArticleFailureAction,
+  createArticleSuccessAction
+} from '../actions/create-article.actions'
+import { CreateArticleService } from '../../services/create-article.service'
+import { ArticleInterface } from '../../../shared/interfaces/article.interface'
+import { HttpErrorResponse } from '@angular/common/http'
+
+@Injectable()
+export class CreateArticleEffect {
+  createArticle$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(createArticleAction),
+      switchMap(({ articleInput }) => {
+        return this.createArticleService.createArticle(articleInput).pipe(
+          map((article: ArticleInterface) =>
+            createArticleSuccessAction({ article })
+          ),
+          catchError((errorResponse: HttpErrorResponse) =>
+            of(
+              createArticleFailureAction({ errors: errorResponse.error.errors })
+            )
+          )
+        )
+      })
+    )
+  )
+
+  redirectAfterCreate$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(createArticleSuccessAction),
+        tap(({ article }) => {
+          this.router.navigate(['/article', article.slug])
+        })
+      ),
+    { dispatch: false }
+  )
+
+  constructor(
+    private actions$: Actions,
+    private createArticleService: CreateArticleService,
+    private router: Router
+  ) {}
+}
