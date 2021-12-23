90721cff100347c2d1957725b2faca1c72357360
denvysotskyi <denvysotskyi@gmail.com>
2021-12-23 14:07:05 +0200

    Modified edit-article module

--- src/app/edit-article/components/edit-article/edit-article.component.ts
+++ src/app/edit-article/components/edit-article/edit-article.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { filter, map, Observable } from 'rxjs'
+import { select, Store } from '@ngrx/store'
+
+import {
+  articleSelector,
+  isLoadingSelector,
+  isSubmittingSelector,
+  validationErrorsSelector
+} from '../../store/selectors/edit-article.selectors'
+import { getArticleAction } from '../../store/actions/get-article.actions'
+import { createArticleAction } from '../../../create-article/store/actions/create-article.actions'
+import { ArticleInputInterface } from '../../../shared/interfaces/article-input.interface'
+import { BackendErrorsInterface } from '../../../shared/interfaces/backend-errors.interface'
+import { ArticleInterface } from '../../../shared/interfaces/article.interface'
 
 @Component({
   selector: 'app-edit-article',
@@ -6,7 +21,43 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./edit-article.component.sass']
 })
 export class EditArticleComponent implements OnInit {
-  constructor() {}
+  initialValues$: Observable<ArticleInputInterface>
+  isLoading$: Observable<boolean>
+  isSubmitting$: Observable<boolean>
+  backendErrors$: Observable<BackendErrorsInterface | null>
+  slug: string
 
-  ngOnInit(): void {}
+  constructor(private store: Store, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.initializeValues()
+    this.fetchData()
+  }
+
+  initializeValues(): void {
+    this.slug = this.route.snapshot.paramMap.get('slug')
+    this.initialValues$ = this.store.pipe(
+      select(articleSelector),
+      filter(Boolean),
+      map((article: ArticleInterface) => {
+        return {
+          title: article.title,
+          description: article.description,
+          body: article.body,
+          tagList: article.tagList
+        }
+      })
+    )
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector))
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+    this.backendErrors$ = this.store.pipe(select(validationErrorsSelector))
+  }
+
+  fetchData(): void {
+    this.store.dispatch(getArticleAction({ slug: this.slug }))
+  }
+
+  onSubmit(articleInput: ArticleInputInterface): void {
+    this.store.dispatch(createArticleAction({ articleInput }))
+  }
 }
--- src/app/edit-article/edit-article.module.ts
+++ src/app/edit-article/edit-article.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterModule, Routes } from '@angular/router'
 
 import { EditArticleComponent } from './components/edit-article/edit-article.component'
+import { ArticleFormModule } from '../shared/modules/article-form/article-form.module'
+import { LoadingModule } from '../shared/modules/loading/loading.module'
+
+const routes: Routes = [
+  {
+    path: 'articles/:slug/edit',
+    component: EditArticleComponent
+  }
+]
 
 @NgModule({
   declarations: [EditArticleComponent],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ArticleFormModule,
+    LoadingModule
+  ]
 })
 export class EditArticleModule {}
